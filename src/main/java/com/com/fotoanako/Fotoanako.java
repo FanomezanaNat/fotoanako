89c9b6dcecca20aa7206741db219ff56d05bdb92
fanomezana <hei.fanomezana.2@gmail.com>
2026-07-12 20:17:47 +0300

    feat: agenda services

--- src/main/java/com/com/fotoanako/Fotoanako.java
+++ src/main/java/com/com/fotoanako/Fotoanako.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FotoanakoApplication {
+public class Fotoanako {
 
   public static void main(String[] args) {
-    SpringApplication.run(FotoanakoApplication.class, args);
+    SpringApplication.run(Fotoanako.class, args);
   }
 
 }
